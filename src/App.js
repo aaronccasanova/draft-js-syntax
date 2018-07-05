65de24ad3486c845e277c344a279bb674c298cb7
casyjs <1boris231@gmail.com>
2018-07-04 21:09:08 -0700

    editor is functioning perfect at the moment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PrismDecorator from 'draft-js-prism';
 import { Editor, EditorState, RichUtils, convertFromRaw } from 'draft-js';
 
+// Only highlights js if you change defaultSyntax: null to 'javascript' in the nodemodules/draft-js-prism/lib/options
+
 class PrismEditorExample extends Component {
   constructor(props) {
     super(props);
@@ -33,10 +35,13 @@ class PrismEditorExample extends Component {
   _handleKeyCommand(command) {
     const { editorState } = this.state;
     const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
+    // add && newState.length > 0 to check if there is any content in the editor
+    // previous functionality would delete the syntax highlighted editor, which would not re-render without a page refresh
+    if (newState && newState.lenght > 0) {
       this.onChange(newState);
       return true;
     }
+
     return false;
   }
 
@@ -58,14 +63,16 @@ class PrismEditorExample extends Component {
     let className = 'RichEditor-editor';
     var contentState = editorState.getCurrentContent();
     if (!contentState.hasText()) {
-      if (
-        contentState
-          .getBlockMap()
-          .first()
-          .getType() !== 'unstyled'
-      ) {
-        className += ' RichEditor-hidePlaceholder';
-      }
+      // After _handleKeyCommand fix, the code below would not show the placeholder text
+      // now when then editor is empty the placeholder texts appears
+      // if (
+      //   contentState
+      //     .getBlockMap()
+      //     .first()
+      //     .getType() !== 'unstyled'
+      // ) {
+      //   className += ' RichEditor-hidePlaceholder';
+      // }
     }
 
     return (
@@ -75,7 +82,7 @@ class PrismEditorExample extends Component {
             editorState={editorState}
             handleKeyCommand={this.handleKeyCommand}
             onChange={this.onChange}
-            placeholder="Tell a story..."
+            placeholder="Add Component Code"
             ref="editor"
             spellCheck={true}
           />
